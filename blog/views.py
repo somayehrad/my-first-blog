fc03b35163c0e5be71d4f1558a4c2f5013eb70c9
somayehrad <somayeh.navabirad@gmail.com>
2021-09-26 03:34:00 +0330

    f3

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.utils import timezone
 # Create your views here.
+def post_list(request):
+    return HttpResponse("Hello Django")
+
+def view_date(request):
+    return HttpResponse(str(timezone.now()))
